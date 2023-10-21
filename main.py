55df945bdf93b38b90a986dd5ed381314e45204a
Caleb <calebkan1106@gmail.com>
2023-10-21 20:56:42 +0100

    version 1.6

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ llm = ChatOpenAI(temperature=0, model="gpt-4")
 
 def main():
     
-    st.set_page_config(page_title="Solidity to Clarity", page_icon=":robot_face:")
+    st.set_page_config(page_title="Solidity to Clarity", page_icon=":computer:")
 
     with st.sidebar:
         st.title("Solidity to Clarity Converter")
@@ -39,16 +39,19 @@ def main():
     
     llm_chain = LLMChain(prompt=prompt, llm=llm)
     
-    uploaded_file = st.file_uploader("Choose a file")
+    uploaded_file = st.file_uploader("Upload `.sol` files here", type="sol")
     if uploaded_file is not None:
         
         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
         string_data = stringio.read()
+        
         query = "Convert the Solidity Code into Clarity Code \n" + string_data
         
         with get_openai_callback() as cb:
-            st.code(llm_chain.run(query), language = 'clarity')
-            st.code("This message costed $" + str(round(cb.total_cost, 4)), 'python')
+            response = llm_chain.run(query)
+            st.code(response, language='clarity')
+            st.code("This message costed $" + str(round(cb.total_cost, 4)), language='python')
+            st.download_button(label=":page_facing_up: Download code as `.clar`", data=response, file_name=(str(uploaded_file.name) + ".clar"))
 
 if __name__ == "__main__":
     main()
